d53324ba622c167aaae94249ee0a57fbd7fe0100
EZ <bloominglamp14@gmail.com>
2019-07-14 12:31:33 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Elizabeth Zietz on 7/14/19.
+//  Copyright © 2019 Elizabeth Zietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Elizabeth Zietz on 7/14/19.
+//  Copyright © 2019 Elizabeth Zietz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
